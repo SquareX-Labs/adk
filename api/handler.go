7adbb2e76c3c0c8b4797d50688885160a2c326fa
Manigandan Dharmalingam <manigandan.jeff@gmail.com>
2023-11-02 16:51:07 +0530

    notifier: hijack req body and include in the slack msg

--- api/handler.go
+++ api/handler.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 
@@ -22,9 +23,15 @@ func (fn Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// that will be logged in logger middleware.
 	// use logger middleware at the root of the router chain as possible
 
+	url := fmt.Sprintf("%s %s?%s", r.Method, r.URL.Path, r.URL.RawQuery)
+	// hijack the request body and enclose again
+	body, err := hijackBody(r)
+	if err != nil {
+		notifySlack(url, body, err)
+	}
+
 	if err := fn(w, r); err != nil {
-		url := fmt.Sprintf("%s %s?%s", r.Method, r.URL.Path, r.URL.RawQuery)
-		notifySlack(url, err)
+		notifySlack(url, body, err)
 		// TODO: handle 5XX, notify developers. Configurable
 		if fErr := respond.Fail(w, err); fErr.NotNil() {
 			log.Printf("[panic] failed to write response. [%s] %s [%d] %s?%s",
@@ -43,7 +50,7 @@ func getReqID(r *http.Request) string {
 }
 
 // notifySlack notifies the slack channel with the error message
-func notifySlack(url string, err *errors.AppError) {
+func notifySlack(url string, body []byte, err *errors.AppError) {
 	if slackHookInfo == "" {
 		return
 	}
@@ -55,10 +62,43 @@ func notifySlack(url string, err *errors.AppError) {
 	prefix := fmt.Sprintf("%s ATTENTION %s %d", ServiceName, errType, err.GetStatus())
 	message := err.Error()
 
-	errStr := fmt.Sprintf("%s: \n```%s\n%s```", prefix, url, message)
+	errStr := fmt.Sprintf("%s: \n```Path: %s\n Payload: %s\n\n Msg: %s```", prefix, url, string(body), message)
 	data := map[string]string{"text": errStr}
 
 	jsonData, _ := json.Marshal(data)
 
 	http.Post(slackHookInfo, "application/json", bytes.NewBuffer(jsonData))
+}
+
+var hijackMethods = map[string]bool{
+	http.MethodPost: true,
+	http.MethodPut:  true,
+}
+
+func hijackBody(r *http.Request) ([]byte, *errors.AppError) {
+	var (
+		body []byte
+		err  error
+	)
+
+	if !hijackMethods[r.Method] {
+		return []byte("nil"), nil
+	}
+	if r.Body == nil {
+		return []byte("nil"), nil
+	}
+	body, err = io.ReadAll(r.Body)
+	if err != nil {
+		msg := fmt.Sprintf("[ADK Notifier] HijackRequestBody: read body %s\n", err.Error())
+		return []byte("nil"), errors.InternalServer(msg)
+	}
+
+	if err := r.Body.Close(); err != nil {
+		msg := fmt.Sprintf("[ADK Notifier] HijackRequestBody: close body, %s", err.Error())
+		return []byte("nil"), errors.InternalServer(msg)
+	}
+
+	r.Body = io.NopCloser(bytes.NewBuffer(body))
+
+	return body, nil
 }
